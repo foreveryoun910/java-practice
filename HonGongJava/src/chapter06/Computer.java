def6f417784d443a60505170e3a4bc5df3d531ef
foreveryoun910 <foreveryoun910@gmail.com>
2021-06-23 23:47:51 +0900

    210623

---
+++ HonGongJava/src/chapter06/Computer.java
@@ -0,0 +1,21 @@
+package chapter06;
+
+//매개변수의 개수를 모를 경우 -> 배열타입으로 선언한다
+public class Computer {
+	
+	int sum1(int[] values) {
+		int sum = 0;
+		for(int i=0; i<values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+	
+	int sum2(int ... values) {
+		int sum = 0;
+		for(int i=0; i<values.length; i++) {
+			sum += values[i];
+		}
+		return sum;
+	}
+}
