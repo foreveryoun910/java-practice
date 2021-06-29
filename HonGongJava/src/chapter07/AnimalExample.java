4839ace1873e97faed5357a94f203b9c6d7ff725
foreveryoun910 <foreveryoun910@gmail.com>
2021-06-29 23:44:07 +0900

    210629 추상 클래스

---
+++ HonGongJava/src/chapter07/AnimalExample.java
@@ -0,0 +1,28 @@
+package chapter07;
+
+public class AnimalExample {
+	public static void main(String[] args) {
+		
+		Dog dog = new Dog();
+		Cat cat = new Cat();
+		dog.sound();
+		cat.sound();
+		System.out.println("-----");
+		
+		//변수의 자동 타입 변환
+		Animal animal = null;
+		animal = new Dog();	//자식은 부모 타입으로 자동 타입 변환
+		animal.sound();
+		animal = new Cat();
+		animal.sound();
+		System.out.println("-----");
+		
+		//메소드의 다형성
+		animalSound(new Dog());
+		animalSound(new Cat());
+	}
+	
+	public static void animalSound(Animal animal) {
+		animal.sound();
+	}
+}
